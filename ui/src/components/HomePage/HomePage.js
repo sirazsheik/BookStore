447ac60caf8a6d28f710a5a11803b53c044e0a08
rajeshgutthula <rajeshgutthula888@gmail.com>
2024-06-22 17:34:20 +0530

    ntng

--- ui/src/components/HomePage/HomePage.js
+++ ui/src/components/HomePage/HomePage.js
@@ -3,22 +3,23 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 const HomePage=()=>{
     return(
-        <div>
+        <div className="secound-half">
             <nav className="navbar">
                 <div className="container-fluid">
                     <a className="navbar-brand" href="$"><img className="brandlogo" src="/images/Homelogo1.png" alt="homelogo"/></a>
                     <form className="formitems">
-                        <h6 class="home-heading">Home</h6>
-                        <h6 class="home-heading2">Bookshelves</h6>
+                        <h6 className="home-heading">Home</h6>
+                        <h6 className="home-heading2">Bookshelves</h6>
                         <button className="home-heading3" type="submit">logout</button>
                     </form>
                 </div>
             </nav>
             <div>
-                
+                <h5 className="home-heading4">Find Your Next Favorite Books?</h5>
             </div>
         </div>
     )
+    
 }
 
 export default HomePage;
